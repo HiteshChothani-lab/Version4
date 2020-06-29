598e04c554e69151416ea69d85102f6d91e1892f
Hitesh Chothani <android.chothani@hotmail.com>
2020-06-29 21:30:38 +0530

    MainTableListView_CollectionChanged event added.

--- UserManagement.UI/Views/MainPage.xaml.cs
+++ UserManagement.UI/Views/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Specialized;
+using System.Windows.Controls;
 
 namespace UserManagement.UI.Views
 {
@@ -10,6 +11,17 @@ namespace UserManagement.UI.Views
         public MainPage()
         {
             InitializeComponent();
+            ((INotifyCollectionChanged)MainTableListView.Items).CollectionChanged += MainTableListView_CollectionChanged;
+        }
+
+        private void MainTableListView_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            try
+            {
+                if (MainTableListView != null && MainTableListView.Items != null && MainTableListView.Items.Count > 1)
+                    MainTableListView.ScrollIntoView(MainTableListView.Items[MainTableListView.Items.Count - 1]);
+            }
+            catch { }
         }
     }
 }
